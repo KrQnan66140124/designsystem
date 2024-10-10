92be052bfba866bc6bbd185f78db50e6d5ff1782
KrQnan <kevin.ramlinger@forsakringskassan.se>
2024-10-10 10:40:02 +0200

    refactor: added useSlotUtils and refactor FCard (refs SFKUI-6500)

--- packages/vue/src/components/FCard/FCard.vue
+++ packages/vue/src/components/FCard/FCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="card card--default">
-        <div v-if="$slots.header" class="card__header">
+        <div v-if="hasHeaderSlot" class="card__header">
             <!--@slot Slot for the title. -->
-            <slot name="header" v-bind="{ headingSlotClass: headingClass }"></slot>
+            <slot name="header" class="card__header-label"></slot>
         </div>
         <div class="card__content">
             <!--@slot Slot for the main content, e.g. paragraphs, input fields, etc. -->
             <slot name="default"></slot>
         </div>
-        <div v-if="$slots.footer" class="card__footer">
+        <div v-if="hasFooterSlot" class="card__footer">
             <!--@slot Slot the footer content, i.e. buttons. -->
             <slot name="footer"></slot>
         </div>
@@ -16,7 +16,11 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed } from "vue";
+import { useSlotUtils } from "./useSlotUtils";
 
-const headingClass = ref(["card__header-label"]);
+const { hasSlot } = useSlotUtils();
+const hasHeaderSlot = computed(() => hasSlot("header"));
+const hasFooterSlot = computed(() => hasSlot("footer"));
+
 </script>
